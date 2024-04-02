aa00776ae45ddf04d432f0a6db0bce8b58ea016b
aautin <ezagof@gmail.com>
2024-04-02 16:55:25 +0200

    removeLetters finally working (too slow though)

--- removeLetters.c
+++ removeLetters.c
@@ -3,67 +3,72 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-
-int ft_strchr(char *s, char a) {
+char *ft_strchr(char *s, char a) {
     while (*s) {
         if (*s == a)
-            return (1);
+            return (s);
         s++;
     }
-    return (0);
+    return (NULL);
 }
 
-int findThem(char *s, char *toPlace) {
-    for (char i = 0; i < 26; i++) {
-        if (toPlace[i] && ft_strchr(s, i + 'a') == 0)
-            return (0);
-        i++;
-    }
-    return (1);
+int	isEmpty(char toPlace[], int size) {
+	while (size--) {
+		if (toPlace[size])
+			return (0);
+	}
+	return (1);
 }
 
-int isEmpty(char *s, int size) {
-    while (size--) {
-        if (*s)
-            return (0);
-        s++;
-    }
-    return (1);
+int	findThem(char toPlace[], char s[], char a) {
+	int i = 0;
+	int j;
+	while (s[i]) {
+		if (s[i] == a) {
+			j = 0;
+			while (j < 26) {
+				if (toPlace[j] == 1 && ft_strchr(&s[i], j + 'a') == NULL)
+					break ;
+				j++;
+			}
+			if (j == 26)
+				return (i);
+		}
+		i++;
+	}
+	return (-1);
 }
 
-int tryPlace(char *s, char *toPlace, char a) {
-    for (int i = 0; s[i]; i++) {
-        if (s[i] == a && findThem(s + i, toPlace))
-            return (i + 1);
-    }
-    return (0);
-}
-
-char *removeDuplicateLetters(char *s) {
+char	*removeDuplicateLetters(char *s) {
     char    toPlace[26] = "";
+	char	*res = s;
 
-    // store the letters to place in the final result
+    // store the letters to place in final result
     int i = 0;
     while (s[i]) {
         toPlace[s[i++] - 'a'] = 1;
     }
 
-    int placeStatus;
-    while (!isEmpty(toPlace, 26)) {
-        for (i = 0; i < 26; i++) {
-            if (toPlace[i] == 1) {
-                placeStatus = tryPlace(s, toPlace, 'a' + i);
-                if (placeStatus) {
-                    s = s + placeStatus;
-                    printf("i=%d|status=%d|%c|%s\n", i, placeStatus, i + 'a', s);
-                    toPlace[i] = 0;
-                    i = 26;
-                }
-            }
-        }
+	// select the letters to place in the right order
+    char	*placeStatus;
+    int 	resLen = 0;
+	while (!isEmpty(toPlace, 26)) {
+		i = 0;
+		while (i < 26) {
+			if (toPlace[i] == 1) {
+				int index = findThem(toPlace, s, i + 'a');
+				if (index >= 0) {
+					s = s + index + 1;
+					toPlace[i] = 0;
+					res[resLen++] = i + 'a';
+					break ;
+				}
+			}
+			i++;
+		}
     }
-
-    return s;
+	res[resLen] = 0;
+    return res;
 }
 
 int main(void) {
@@ -71,5 +76,6 @@ int main(void) {
 	char *res;
 
 	res = removeDuplicateLetters(str);
+	printf("%s\n", res);
 	return (0);
 }
